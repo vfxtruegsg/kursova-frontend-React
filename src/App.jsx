1bdba9394666405431f37e6f84519505ef525a0c
Andrew <andrejpopkov565@gmail.com>
2025-05-20 11:57:44 +0300

    first settings repository

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <main>
+      <Routes>
+        <Route path="/" />
+        <Route path="/" />
+        <Route path="/" />
+      </Routes>
+    </main>
+  );
+}
+
+export default App;
