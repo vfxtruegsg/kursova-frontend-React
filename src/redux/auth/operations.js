aab2f25811a273fed6899dc3f661173501fcde2d
Andrew <andrejpopkov565@gmail.com>
2025-06-02 13:26:58 +0300

    added auth store settings

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,6 +1,101 @@
-import { baseURL } from '../../utils/axiosDefaultSettings.js';
+import {
+  baseURL,
+  deleteAuthHeader,
+  setAuthHeader,
+} from '../../utils/axiosDefaultSettings.js';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import {
   showToastErrorMessage,
   showToastSuccessMessage,
 } from '../../utils/toastMessages.js';
+
+export const registerUserThunk = createAsyncThunk(
+  'auth/register',
+  async (credentials) => {
+    try {
+      await baseURL.post('/auth/register', credentials);
+
+      return showToastSuccessMessage(`Successfully register a user!`);
+    } catch (error) {
+      if (error.status === 409) {
+        return showToastErrorMessage('User already exist! Please log in!');
+      }
+
+      return showToastErrorMessage(
+        'Something went wrong, please try again later!'
+      );
+    }
+  }
+);
+
+export const loginUserThunk = createAsyncThunk(
+  'auth/login',
+  async (credentials, thunkApi) => {
+    try {
+      const data = await baseURL.post('/auth/login', credentials, {
+        withCredentials: true,
+      });
+
+      setAuthHeader(data.accessToken);
+
+      showToastSuccessMessage(`Welcome, ${data.first_name} ${data.last_name}!`);
+
+      return data;
+    } catch (error) {
+      if (error.status === 409) {
+        showToastErrorMessage('Error, user with this email not exist!');
+        return thunkApi.rejectWithValue(
+          'Error, user with this email not exist!'
+        );
+      }
+
+      showToastErrorMessage('Something went wrong, please try again later!');
+
+      return thunkApi.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const logoutUserThunk = createAsyncThunk(
+  'auth/logout',
+  async (_, thunkApi) => {
+    const token = thunkApi.getState().auth.token;
+    try {
+      const data = await baseURL.post(
+        '/auth/logout',
+        {},
+        { headers: { Authorization: `Bearer ${token}` }, withCredentials: true }
+      );
+
+      showToastSuccessMessage('Successfully logout!');
+
+      return data;
+    } catch (error) {
+      showToastErrorMessage('Something went wrong, please try again later!');
+
+      return thunkApi.rejectWithValue(error.message);
+    } finally {
+      deleteAuthHeader();
+    }
+  }
+);
+
+export const refreshUserThunk = createAsyncThunk(
+  'auth/refresh',
+  async (_, thunkApi) => {
+    const token = thunkApi.getState().auth.token;
+    if (token === null) return thunkApi.rejectWithValue('Token is not exist');
+    try {
+      const data = await baseURL.post(
+        '/auth/refresh',
+        {},
+        { withCredentials: true }
+      );
+
+      setAuthHeader(data.accessToken);
+      return data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
+    }
+  }
+);
--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,11 +1,69 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
+import {
+  loginUserThunk,
+  logoutUserThunk,
+  refreshUserThunk,
+} from './operations.js';
 
-const initialState = {};
+const initialState = {
+  user: {
+    userId: null,
+    firstName: null,
+    lastName: null,
+    email: null,
+  },
+  token: null,
+  isLoggedIn: false,
+  isAuthLoading: false,
+};
 
 const slice = createSlice({
   name: 'auth',
   initialState,
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(loginUserThunk.fulfilled, (state, action) => {
+        state.user.userId = action.payload.id;
+        state.user.firstName = action.payload.first_name;
+        state.user.lastName = action.payload.last_name;
+        state.user.email = action.payload.email;
+        state.token = action.payload.accessToken;
+        state.isAuthLoading = false;
+        state.isLoggedIn = true;
+      })
+
+      .addCase(refreshUserThunk.fulfilled, (state, action) => {
+        state.token = action.payload.accessToken;
+      })
+
+      .addCase(logoutUserThunk.fulfilled, () => {
+        return initialState;
+      })
+
+      .addMatcher(
+        isAnyOf(
+          loginUserThunk.pending,
+          refreshUserThunk.pending,
+          logoutUserThunk.pending
+        ),
+        (state) => {
+          state.isAuthLoading = true;
+          state.isLoggedIn = false;
+        }
+      )
+
+      .addMatcher(
+        isAnyOf(
+          loginUserThunk.rejected,
+          refreshUserThunk.rejected,
+          logoutUserThunk.rejected
+        ),
+        (state) => {
+          state.isAuthLoading = false;
+          state.isLoggedIn = false;
+        }
+      );
+  },
 });
 
 export default slice.reducer;
